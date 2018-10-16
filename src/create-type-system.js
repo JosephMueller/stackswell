2fd9f7d48be670ad2807b3dd504e3f2864a57248
psych0der <mayankbhola@gmail.com>
2018-10-16 19:05:22 +0530

    Undo unintentional typo

--- src/create-type-system.js
+++ src/create-type-system.js
@@ -170,7 +170,7 @@ function create_dialog(context) {
 	// 		'1.200 Minor Third',
 	// 		'1.250 Major Third',
 	// 		'1.333 Perfect Fourth',
-	// 		'1.414 Augmented th',
+	// 		'1.414 Augmented Fourth',
 	// 		'1.500 Perfect Fifth',
 	// 		'1.6 Minor Sixth',
 	// 		'1.618 Golden Ratio',
