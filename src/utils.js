2bfe65086b6d693be463a9647231008591db1115
Mark Horgan <public@markhorgan.com>
2018-11-21 09:45:56 +0000

    Removed Utils.is_equal as it's not being used

--- src/utils.js
+++ src/utils.js
@@ -4,20 +4,4 @@ export default class Utils {
         return JSON.parse(JSON.stringify(object));
     }
 
-    static is_equal(object1, object2) {
-        if (object1.constructor === Array && object2.constructor === Array) {
-            if (object1.length !== object2.length) {
-                return false;
-            }
-            for (let i = 0; i < object1.length; i++) {
-                if (object1[i] != object2[i]) {
-                    return false;
-                }
-            }
-            return true;
-        } else {
-            return object1 == object2;
-        }
-    }
-
 }
